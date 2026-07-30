1bfc6b5cac4b52e959fc6697463ec60342684335
Jose Vicente <josevlopezexposito@gmail.com>
2026-07-30 09:26:16 +0200

    fix: add fallback supabase credentials in client.ts to fix Vercel static prerender build error

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -6,7 +6,12 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  'https://mnfxjxorffxnuxpdzzzd.supabase.co'
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZnhqeG9yZmZ4bnV4cGR6enpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxODQzNDIsImV4cCI6MjEwMDc2MDM0Mn0.EvAmPcHsgIzJLhFZUlPfp9ujjQr2OfXX4ANnIZifdAc'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
